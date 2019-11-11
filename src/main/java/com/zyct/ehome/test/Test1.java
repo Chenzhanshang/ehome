448f7605a344924bc1e19518b466e973c95f7dd3
Chenzhanshang <642125256@qq.com>
2019-11-11 19:20:41 +0800

    2

---
+++ src/main/java/com/zyct/ehome/test/Test1.java
@@ -0,0 +1,9 @@
+package com.zyct.ehome.test;
+
+import com.zyct.ehome.entity.Manage;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Manage manage = new Manage();
+    }
+}
