50971073496e4c89bb59fd0b49d1188b4f1b6e7e
MyGitHubNumber <1945282561@qq.com>
2019-12-16 16:30:36 +0800

    录入候选人列表基本完成2

--- src/main/java/com/zyct/ehome/service/impl/OwnerManageServiceImpl.java
+++ src/main/java/com/zyct/ehome/service/impl/OwnerManageServiceImpl.java
@@ -1,11 +1,14 @@
 package com.zyct.ehome.service.impl;
 
+import com.zyct.ehome.dao.CandidateMapper;
 import com.zyct.ehome.dao.OwnerMapper;
+import com.zyct.ehome.entity.Candidate;
 import com.zyct.ehome.entity.Owner;
 import com.zyct.ehome.service.OwnerManageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,8 +22,30 @@ public class OwnerManageServiceImpl implements OwnerManageService {
     @Autowired
     private OwnerMapper ownerMapper;
 
+    @Autowired
+    private CandidateMapper candidateMapper;
+
     @Override
-    public List<Owner> ownerList(String communityId) {
-        return ownerMapper.getOwnerListByCommunityId(communityId);
+    public List<Owner> unCandidateOwnerList(String communityId) {
+        //获取小区的所有业主
+        List<Owner> ownerListByCommunityId = ownerMapper.getOwnerListByCommunityId(communityId);
+        //获取候选人列表
+        List<Candidate> candidateListByCommunityId = candidateMapper.getCandidateListByCommunityId(communityId);
+        List<Owner> ownerList = new ArrayList<>();
+        for (Owner o:ownerListByCommunityId){
+            boolean flag = true;
+            for (Candidate c:candidateListByCommunityId){
+                if(o.getOwnerId().equals(c.getOwner().getOwnerId())){
+                    //如果该业主是候选人列表中的人则将标志位设为false
+                    flag = false;
+                }
+            }
+            if (flag){
+                //经历了整个循环标志位依旧为true
+                ownerList.add(o);
+            }
+        }
+
+        return ownerList;
     }
 }
