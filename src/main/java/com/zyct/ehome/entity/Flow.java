ff9ed2f27a5172f9af7689825fb76964a2f7a0a9
MyGitHubNumber <1945282561@qq.com>
2019-12-23 14:26:14 +0800

    流程实体类

---
+++ src/main/java/com/zyct/ehome/entity/Flow.java
@@ -0,0 +1,14 @@
+package com.zyct.ehome.entity;
+
+import lombok.Data;
+
+/**
+ * @author JGZ
+ * CreateTime 2019/12/23 9:29
+ * Email 1945282561@qq.com
+ */
+@Data
+public class Flow {
+    private Integer flowId;
+    private String flowName;
+}
